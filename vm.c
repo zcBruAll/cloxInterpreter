951bd3c052ca7edac5f975ae2d25261de642892e
Allan Brunner <allan@allanbrunner.dev>
2025-11-15 18:59:57 +0100

    Completed "Hash Tables" chapter

--- vm.c
+++ vm.c
@@ -8,6 +8,7 @@
 #include "debug.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -31,9 +32,13 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.strings);
 }
 
-void freeVM() { freeObjects(); }
+void freeVM() {
+  freeTable(&vm.strings);
+  freeObjects();
+}
 
 void push(Value value) {
   *vm.stackTop = value;
