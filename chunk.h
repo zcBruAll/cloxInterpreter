72d6bfb7e81cd6c5766ae9ef98b4f058826428e8
Allan Brunner <allan@allanbrunner.dev>
2026-06-24 21:07:16 +0200

    Completed "Classes and Instances" chapter

--- chunk.h
+++ chunk.h
@@ -15,6 +15,8 @@ typedef enum {
   OP_GET_GLOBAL,
   OP_GET_UPVALUE,
   OP_SET_UPVALUE,
+  OP_GET_PROPERTY,
+  OP_SET_PROPERTY,
   OP_DEFINE_GLOBAL,
   OP_SET_GLOBAL,
   OP_EQUAL,
@@ -34,6 +36,7 @@ typedef enum {
   OP_CLOSURE,
   OP_CLOSE_UPVALUE,
   OP_RETURN,
+  OP_CLASS,
 } OpCode;
 
 typedef struct {
--- memory.c
+++ memory.c
@@ -4,6 +4,7 @@
 #include "compiler.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -95,6 +96,12 @@ static void blackenObject(Obj *object) {
     markArray(&function->chunk.constants);
     break;
   }
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    markObject((Obj *)instance->klass);
+    markTable(&instance->fields);
+    break;
+  }
   case OBJ_UPVALUE:
     markValue(((ObjUpvalue *)object)->closed);
     break;
@@ -110,6 +117,11 @@ static void freeObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_CLASS: {
+    ObjClass *klass = (ObjClass *)object;
+    markObject((Obj *)klass->name);
+    break;
+  }
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
     FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
@@ -120,6 +132,12 @@ static void freeObject(Obj *object) {
     ObjFunction *function = (ObjFunction *)object;
     freeChunk(&function->chunk);
     FREE(ObjFunction, object);
+    break;
+  }
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    freeTable(&instance->fields);
+    FREE(ObjInstance, object);
     break;
   }
   case OBJ_NATIVE:
